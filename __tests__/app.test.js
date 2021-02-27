63e789fb1d4b7441755c759527711dee8c36efed
Jake Thrasher <mjakethrasher@gmail.com>
2021-02-26 18:38:14 -0800

    test munge function

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,65 +1,67 @@
 require('dotenv').config();
 
-const { execSync } = require('child_process');
+const { mungeReview } = require('../lib/mungeUtils');
 
-const fakeRequest = require('supertest');
-const app = require('../lib/app');
-const client = require('../lib/client');
+test('returns munged data', async() => {
+  // const lat = 27.33643;
+  // const lon = -82.5306;
+  
+  const expectation = [
+    {
+      'name': 'Owens Fish Camp',
+      'image_url': 'https://s3-media3.fl.yelpcdn.com/bphoto/cIEYVhc7dMB-vIh_Tvv9gQ/o.jpg',
+      'price': '$$',
+      'rating': 4.5,
+      'url': 'https://www.yelp.com/biz/owens-fish-camp-sarasota?adjust_creative=iB-g85KitIPLvFXHo4hWFg&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=iB-g85KitIPLvFXHo4hWFg'
+    }
+  ];
 
-describe('app routes', () => {
-  describe('routes', () => {
-    let token;
-  
-    beforeAll(async done => {
-      execSync('npm run setup-db');
-  
-      client.connect();
-  
-      const signInData = await fakeRequest(app)
-        .post('/auth/signup')
-        .send({
-          email: 'jon@user.com',
-          password: '1234'
-        });
-      
-      token = signInData.body.token; // eslint-disable-line
-  
-      return done();
-    });
-  
-    afterAll(done => {
-      return client.end(done);
-    });
-
-    test('returns animals', async() => {
-
-      const expectation = [
-        {
-          'id': 1,
-          'name': 'bessie',
-          'coolfactor': 3,
-          'owner_id': 1
+  const mungie = {
+    'businesses': [
+      {
+        'id': 'USq57A18KfPeheWDfFzVkA',
+        'alias': 'owens-fish-camp-sarasota',
+        'name': 'Owens Fish Camp',
+        'image_url': 'https://s3-media3.fl.yelpcdn.com/bphoto/cIEYVhc7dMB-vIh_Tvv9gQ/o.jpg',
+        'is_closed': false,
+        'url': 'https://www.yelp.com/biz/owens-fish-camp-sarasota?adjust_creative=iB-g85KitIPLvFXHo4hWFg&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=iB-g85KitIPLvFXHo4hWFg',
+        'review_count': 1656,
+        'categories': [
+          {
+            'alias': 'seafood',
+            'title': 'Seafood'
+          }
+        ],
+        'rating': 4.5,
+        'coordinates': {
+          'latitude': 27.3319,
+          'longitude': -82.53974
         },
-        {
-          'id': 2,
-          'name': 'jumpy',
-          'coolfactor': 4,
-          'owner_id': 1
+        'transactions': [
+          'delivery'
+        ],
+        'price': '$$',
+        'location': {
+          'address1': '516 Burns Ln',
+          'address2': '',
+          'address3': '',
+          'city': 'Sarasota',
+          'zip_code': '34236',
+          'country': 'US',
+          'state': 'FL',
+          'display_address': [
+            '516 Burns Ln',
+            'Sarasota, FL 34236'
+          ]
         },
-        {
-          'id': 3,
-          'name': 'spot',
-          'coolfactor': 10,
-          'owner_id': 1
-        }
-      ];
+        'phone': '+19419516936',
+        'display_phone': '(941) 951-6936',
+        'distance': 1029.578186539982
+      }
+    ]
+  };
 
-      const data = await fakeRequest(app)
-        .get('/animals')
-        .expect('Content-Type', /json/)
-        .expect(200);
-
-      expect(data.body).toEqual(expectation);
-    });
-  });
+  const actual = mungeReview(mungie);
+  expect(actual).toEqual(expectation);
 });
+
